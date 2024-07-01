b72e9951fb25db31a958baacff68de2bffa1f21f
raymond <taraymondl1@gmail.com>
2024-06-30 18:56:09 -0700

    Finished opacity extra credit
    Finished the extra credit assigment added 10% opacity on each hover.
    This is done by appending classes of increasing opacity until maximum is
    reached.

--- etch_sketch.js
+++ etch_sketch.js
@@ -2,6 +2,42 @@ const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".resetBtn");
 
+function setOpacity(node) {
+    const lastClassIndex = node.classList.length - 1;
+    const className = node.classList[lastClassIndex];
+
+    if (className === "zeroPercent") {
+        node.classList.add("tenPercent");
+    }
+    else if (className === "tenPercent") {
+        node.classList.add("twentyPercent");
+    }
+    else if (className === "twentyPercent") {
+        node.classList.add("thirtyPercent");
+    }
+    else if (className === "thirtyPercent") {
+        node.classList.add("fortyPercent");
+    }
+    else if (className === "fortyPercent") {
+        node.classList.add("fiftyPercent");
+    }
+    else if (className === "fiftyPercent") {
+        node.classList.add("sixtyPercent");
+    }
+    else if (className === "sixtyPercent") {
+        node.classList.add("seventyPercent");
+    }
+    else if (className === "seventyPercent") {
+        node.classList.add("eightyPercent");
+    }
+    else if (className === "eightyPercent") {
+        node.classList.add("ninetyPercent");
+    }
+    else if (className === "ninetyPercent") {
+        node.classList.add("hundredPercent");
+    }
+}
+
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const miniContainer = document.createElement("div");
@@ -9,11 +45,10 @@ function createGrid(gridSize) {
         container.appendChild(miniContainer);
         for (let j = 0; j < gridSize; j++) {
             const box = document.createElement("div");
-            box.classList.add("pixel");
+            box.classList.add("pixel", "zeroPercent");
             miniContainer.appendChild(box);
             box.addEventListener("mouseover", function () {
-                box.style["background-color"] = "black";
-                //box.style["opacity"] += 0.1;
+                setOpacity(box);
             });
         }
     }
