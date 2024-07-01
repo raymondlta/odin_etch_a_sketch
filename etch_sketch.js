73b58c95ca4aba4b262b7030cf7b171d02adddad
raymond <taraymondl1@gmail.com>
2024-06-30 18:26:22 -0700

    Cleaned up css file and reorganized js file
    Removed old comments in the css file for clarity as well as added a
    border for the canvas to view. Moved error message to appear at the top
    of the page and created new functions to make code more readable.

--- etch_sketch.js
+++ etch_sketch.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".resetBtn");
 
@@ -17,8 +18,22 @@ function createGrid(gridSize) {
     }
 }
 
+function createError() {
+    const error = document.createElement("div");
+    error.classList.add("error");
+    error.textContent = "That is not a valid input, try again...";
+    body.insertBefore(error, body.firstChild);
+}
+
+function clearError() {
+    if (body.firstChild.className === "error") {
+        body.removeChild(body.firstChild);
+    }
+}
+
 function clearGrid() {
     const range = container.children.length;
+
     for (let i = 0; i < range; i++) {
         container.removeChild(container.lastChild);
     }
@@ -28,14 +43,12 @@ resetBtn.addEventListener("click", function () {
     const size = parseInt(prompt("Enter an integer from 0 to 100: "));
 
     if (size < 0 || size > 100 || !size) {
-        if (container.lastChild.className !== "error") {
-            const error = document.createElement("div");
-            error.classList.add("error");
-            error.textContent = "That is not a valid input, try again...";
-            container.appendChild(error);
+        if (body.firstChild.className !== "error") {
+            createError()
         }
     }
     else {
+        clearError();
         clearGrid();
         createGrid(size);
     }
